12ff245b1ab69145de2d5732b0d54338aa2a0e90
Carsten Bund <carstenbund@gmail.com>
2026-06-04 23:28:45 +0000

    bump version add time sync with browser and display from esp32

--- http_controller/http_controller.ino
+++ http_controller/http_controller.ino
@@ -5,7 +5,7 @@
 #include <SPIFFS.h>
 
 char Project[20] = "mementumLED";
-char Version[20] = "1.4.3";
+char Version[20] = "1.4.4";
 
 char apSSID[64] = "ESP32-S3-Matrix";
 char apPSK[64]  = "waveshare";
